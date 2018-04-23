09cf30e073e97e106ef8a868300ff69a13ed4038
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DevExpress.XtraReports.UI;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        XtraReport report = new XtraReport1();
+
+        report.CreateDocument();
+        ASPxPager1.ItemCount = report.Pages.Count;
+        DocumentViewer1.Report = report;
+    }
+
+    protected void ASPxPager1_PageIndexChanged(object sender, EventArgs e) {
+        HiddenField1.Value = ASPxPager1.PageIndex.ToString();
+    }
+}
